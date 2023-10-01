f1dcf4d56436d48feb553e94ac06063279ce442a
vietvanba <baviet19@gmail.com>
2023-10-01 23:30:15 +0700

    add NotFoundOxford exception

--- src/main/java/com/dictionary/VietDictionary/service/SearchService.java
+++ src/main/java/com/dictionary/VietDictionary/service/SearchService.java
@@ -1,30 +1,33 @@
 package com.dictionary.VietDictionary.service;
 
+import com.dictionary.VietDictionary.exception.NotFoundOxford;
 import com.dictionary.VietDictionary.payload.SearchResponse;
 import com.dictionary.VietDictionary.repository.SearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.io.*;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
     private final SearchRepository searchRepository;
     private final OxfordDictionaryService oxfordDictionaryService;
+
     public SearchResponse extractDataFromOxford(String word) {
-        return searchRepository.save(oxfordDictionaryService.fetchWordDefinition(word));
+        try {
+            return searchRepository.save(oxfordDictionaryService.fetchWordDefinition(word));
+        } catch (Exception e) {
+            throw new NotFoundOxford(word, e);
+        }
     }
 
     public SearchResponse findByWord(String word) {
         SearchResponse response = searchRepository.findById(word).orElse(null);
-        if(response==null)
-        {
-            response=extractDataFromOxford(word);
+        if (response == null) {
+            response = extractDataFromOxford(word);
         }
         return response;
     }
+
     public SearchResponse findByWord(String word, Integer type) {
         SearchResponse response = null;
         if (type.equals(1)) {
